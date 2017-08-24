7eed78692c1914a479e5506bab325991b72d3a17
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-08-25 00:22:17 +0530

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int length,breadth,per;
+cout << "Enter length:" << endl;
+cin >> length;
+cout << "Enter breadth:" << endl;
+cin >> breadth;
+per=2*(length+breadth);
+ cout << "Perimeter=" << per;
+	return 0;
+}
