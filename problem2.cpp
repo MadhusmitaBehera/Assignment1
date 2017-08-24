5633df6255595f61ad518a0e314c5bf636c698e2
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-08-25 00:15:50 +0530

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,b,sum;
+cout << "Enter two numbers:" << endl;
+cin >> a;
+cin >> b;
+sum=a+b;
+cout << "Sum=" << sum;
+ 
+	return 0;
+}
