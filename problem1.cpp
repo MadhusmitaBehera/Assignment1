5503913008d52bb4d6092e91bf8ff5fe929ebd33
MadhusmitaBehera <31300575+MadhusmitaBehera@users.noreply.github.com>
2017-08-25 00:12:56 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a;
+float b;
+char c;
+double d;
+bool f=false;
+bool t=true;
+cout << "Enter an Integer:" << endl;
+cin >> a;
+cout << "Your Integer=" << a << endl;
+cout << "Enter a Float:" << endl;
+cin >> b;
+cout << "Your float=" << b << endl;
+cout << "Enter a character:" << endl;
+cin >> c;
+cout << "Your character=" << c << endl;
+cout << "Enter a double=" << endl;
+cin >> d;
+cout << "Your Double=" << d << endl;
+cout << f << endl;
+cout << t;  
+	return 0;
+}
